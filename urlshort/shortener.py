921f5227ae735acdc93c6462434208ec5a6fbc16
unknown <ravi.ujjwal2000@gmail.com>
2022-01-20 19:34:44 +0530

    Heroku Commit First

--- urlshort/shortener.py
+++ urlshort/shortener.py
@@ -1,11 +1,17 @@
 import random
-from hashids import Hashids
+import string
 from .models import *
 
-def shortenIt():
-    num = random.randint(1000, 100000)
-    hashes = Hashids(min_length=5)
-    h = hashes.encode(num)
-    if UrlShort.objects.filter(short=h).exists():
+# def shortenIt():
+#     num = random.randint(1000, 100000)
+#     hashes = Hashids(min_length=5)
+#     h = hashes.encode(num)
+#     if UrlShort.objects.filter(short=h).exists():
+#         shortenIt()
+#     return h
+
+def shortenIt(size=7, chars=string.ascii_lowercase + string.digits):
+    temp = ''.join(random.choice(chars) for _ in range(size))
+    if UrlShort.objects.filter(short=temp).exists():
         shortenIt()
-    return h
+    return temp
